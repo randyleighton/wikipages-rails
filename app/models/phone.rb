7b669baa0c1c1b796f1ceb0b3c1f593bfbbd1bbc
randyleighton & jennifermccarthy <student@epicodus.com>
2014-08-25 13:51:28 -0700

    phones belong to a contact association made

--- app/models/phone.rb
+++ app/models/phone.rb
@@ -1,4 +1,6 @@
 class Phone < ActiveRecord::Base
   validates :number, presence: true
   validates :contact_id, presence: true
+
+  belongs_to :contact
 end
--- spec/models/phone_spec.rb
+++ spec/models/phone_spec.rb
@@ -3,4 +3,5 @@ require 'rails_helper'
 describe Phone do
   it { should validate_presence_of :number }
   it { should validate_presence_of :contact_id }
+  it { should belong_to :contact }
 end
