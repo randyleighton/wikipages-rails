48fef2c5be0813d6d0186f5f0d6d0108a8de1fba
randyleighton & jennifermccarthy <student@epicodus.com>
2014-08-25 12:08:31 -0700

    will create new contact, edit, and update and show

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
   match('contacts', {via: :get, to: 'contacts#index'})
+  match('contacts/new', {via: :get, to: 'contacts#new'})
+  match('contacts', {via: :post, to: 'contacts#create'})
+  match('contacts/:id', {via: :get, to: 'contacts#show'})
+  match('contacts/:id/edit', {via: :get, to: 'contacts#edit'})
+  match('contacts/:id', {via: [:patch, :put], to: 'contacts#update'})
 end
